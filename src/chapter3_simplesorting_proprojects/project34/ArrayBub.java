9ced342d0ecfcd87d5811e8029e10e42e89ee235
daniel mullan <dwmullan@gmail.com>
2018-01-11 18:50:31 +0000

    ++statement

--- src/chapter3_simplesorting_proprojects/project34/ArrayBub.java
+++ src/chapter3_simplesorting_proprojects/project34/ArrayBub.java
@@ -1,5 +1,12 @@
 package chapter3_simplesorting_proprojects.project34;
 
+//Another simple sort is the odd-even sort. The idea is to repeatedly make two passes through the array.
+//On the first pass you look at all the pairs of items, a[j] and a[j+1], where j is odd(j= 1,3,5,...)
+//If their key values are out of order, you swap them. On the second pass you do the same for all the even
+//values (j=2,4,6,...). You do these two passes repeatedly until the array is sorted. Replace the bubbleSort()
+//method in bubbleSort.java (Listing 3.1) with an OddEvenSort() method. Make sure it works for varying amounts of data
+//You'll need to figure out how many times to do the two passes.
+
 public class ArrayBub {
 	private long[] a;
 	private int nElems;
